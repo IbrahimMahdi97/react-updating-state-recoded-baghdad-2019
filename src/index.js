4048f5adf536acb90bc9bc315412ea4c23a9493a
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-17 15:14:31 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ClickityCLick from './components/ClickityCLick';
+
 ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
+  <ClickityCLick />,
   document.getElementById('root')
 );
